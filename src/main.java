2bf0db42be4c44df1465ee472fd4ef82d0e324b1
straiffix <straiffix@gmail.com>
2018-05-16 00:51:21 +0200

    some changes

--- src/main.java
+++ src/main.java
@@ -8,59 +8,50 @@ import javafx.stage.Stage;
 
 public class  main extends Application {
 
-    Rectangle [][] grid;
+    Rectangle [][] grid; //Field
+    static final int NUMBER_OF_CELLS = 100;
+    static final int CELL_SIZE=10;
 
     public static void main(String[] args) { Application.launch(args);}
         
     @Override
     public void start(final Stage primaryStage) {
-        primaryStage.setTitle("Animation");
+
+        primaryStage.setTitle("Langton's Ant");
         Group root = new Group();
-        Scene scene = new Scene(root, 1000, 1000, Color.WHITE);
-        final Color WHITE = Color.WHITE;
-        final Color BLACK = Color.BLACK;
-        grid = new Rectangle[100][100];
+        Scene scene = new Scene(root, 1000, 1000, Color.WHITE); //Width = height = size of cell * number of cells
+
+        grid = new Rectangle[NUMBER_OF_CELLS][NUMBER_OF_CELLS]; //Init field
+
         int x = 0, y = 0;
-        for (int i=0; i<100;i++) {
-            for (int j=0; j<100; j++) {
+        for (int i=0; i<NUMBER_OF_CELLS;i++) {
+            for (int j=0; j<NUMBER_OF_CELLS; j++) {
                grid[i][j] = new Rectangle();
-               grid[i][j].setFill(WHITE);
-               grid[i][j].setStroke(BLACK);
+               grid[i][j].setFill(Color.WHITE);
+               grid[i][j].setStroke(Color.BLACK);
                grid[i][j].setStrokeWidth(.8);
                grid[i][j].setX(x);
                grid[i][j].setY(y);
-               grid[i][j].setWidth(10);
-               grid[i][j].setHeight(10);
-               x+=10;
+               grid[i][j].setWidth(CELL_SIZE);
+               grid[i][j].setHeight(CELL_SIZE);
+               x+=CELL_SIZE;
                root.getChildren().add(grid[i][j]);
 
                }
-            y+=10;
+            y+=CELL_SIZE;
             x=0;
          }
 
         primaryStage.setScene(scene);
-
         primaryStage.show();
 
-
-        Automate langton = new Automate(25, 25, 2, Color.BLACK, grid);
-        langton.langtonAnt();
-        Automate langton2 = new Automate(70, 25, 0, Color.RED, grid);
-        langton2.langtonAnt();
-        Automate langton3 = new Automate(25, 70, 1, Color.BLUE, grid);
-        langton3.langtonAnt();
-        Automate langton4 = new Automate(50, 50, 3, Color.LIGHTCORAL, grid);
-        langton4.langtonAnt();
-
-
-
-
-
-
-
+        Ant langton = new Ant(25, 25, 2, Color.GREEN, grid);
+        langton.setAnt();
+        Ant langton2 = new Ant(70, 70, 0, Color.RED, grid);
+        langton2.setAnt();
+        Ant langton3 = new Ant(50, 50, 1, Color.BLUE, grid);
+        langton3.setAnt();
 
     }
-
 
 }

085c26acdd1baacebcecd7ff7819bc39164ffd30
straiffix <straiffix@gmail.com>
2018-05-16 00:52:43 +0200

    some changes

--- src/Ant.java
+++ src/Ant.java
@@ -5,21 +5,26 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 
-public class Automate {
-    static final int SIZE = 100;
-    static final int STEPS=11000;
+public class Ant {
+
+
+    int SIZE = 100; // Size of field
+    int STEPS = 11000;
+    int SPEED = 10; // 2 - fast, 10 - medium, 30 - slow, 60 - very slow;
+
 
     Rectangle[][] grid;
-    private int currentX;
-    private int currentY;
+
+    private int antX;
+    private int antY;
     private int direction;
     private Color antColor;
     private int testSteps=0;
 
-    public Automate(int currentX, int currentY, int direction, Color antColor, Rectangle[][] grid){
+    public Ant(int antX, int antY, int direction, Color antColor, Rectangle[][] grid){
 
-        this.currentX=currentX;
-        this.currentY=currentY;
+        this.antX = antX;
+        this.antY = antY;
         this.direction = direction;
         this.antColor=antColor;
         this.grid=grid;
@@ -28,41 +33,38 @@ public class Automate {
 
     private void repaint(int x, int y, Color color){
         grid[x][y].setFill(color);
-    }
+    } // Change color
 
-    private void antGo(){
+    private void antGo(){ //Change coordinate
 
         if (direction == 0) {
-            currentY++;
+            antY++;
         } else if (direction == 1) {
-            currentX++;
+            antX++;
         } else if (direction == 2) {
-            currentY--;
+            antY--;
         } else {
-            currentX--;
+            antX--;
         }
 
     }
 
     private void step(){
 
-                if (grid[currentX][currentY].getFill().equals(Color.WHITE)) {
-                    if (direction != 3) {
+                if (grid[antX][antY].getFill().equals(Color.WHITE)) {
+                    if (direction != 3) {  //Change direction
                         direction++;
                     } else { direction = 0;}
 
-                    repaint(currentX, currentY, antColor);
-
-
-                    antGo();
+                    repaint(antX, antY, antColor); // Change color
+                    antGo(); //Change place
 
                 } else {
                     if (direction != 0) {
                         direction--;
                     } else { direction = 3;}
 
-                    repaint(currentX, currentY, Color.WHITE);
-
+                    repaint(antX, antY, Color.WHITE);
                     antGo();
 
                 }
@@ -72,18 +74,21 @@ public class Automate {
 
     }
 
-    public void langtonAnt(){
+    public void setAnt(){
 
         Timer timer = new Timer();
         timer.schedule(new TimerTask(){
 
                     @Override
                     public void run(){
+                        try {
                         step();
-                        if(testSteps>=STEPS || currentX<=0 || currentX >= SIZE || currentY <= 0 || currentY >= SIZE){ timer.cancel(); }
+                        if(testSteps>=STEPS || antX <=0 || antX >= SIZE || antY <= 0 || antY >= SIZE) { timer.cancel();}
+                        } catch (ArrayIndexOutOfBoundsException e) { timer.cancel();}
+                            }
                     }
 
-                }, 1000, 5);
+                , 1000, SPEED);
 
         }
 
